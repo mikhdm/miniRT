236d71a670edf70e719f7bd4b48ba00a40adefa9
Dmitry Mikhailenko <d@mikhailenko.net>
2025-09-28 16:05:38 +0200

    ft_atof in progress

--- miniRT/lib/libft/ft_atof.c
+++ miniRT/lib/libft/ft_atof.c
@@ -5,9 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/23 05:06:53 by rmander           #+#    #+#             */
-/*   Updated: 2021/05/23 05:06:54 by rmander          ###   ########.fr       */
+/*   Created: 2021/05/23 05:18:54 by rmander           #+#    #+#             */
+/*   Updated: 2021/05/23 05:21:15 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <math.h>
 
+double  ft_atof(const char *str)
+{
+	long double value;
+	value = INFINITY;
+
+	/* TODO */
+
+	return ((double)value);
+}
