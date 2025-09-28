3b7683c2d038278a39c060b01167006dfdc1d23d
Dmitry Mikhailenko <d@mikhailenko.net>
2025-09-28 16:03:04 +0200

    remove mlx warnings

--- miniRT/src/main.c
+++ miniRT/src/main.c
@@ -6,20 +6,24 @@
 /*   By: rmander <rmander@student.21-school.ru      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 23:37:11 by rmander           #+#    #+#             */
-/*   Updated: 2021/04/10 23:23:59 by rmander          ###   ########.fr       */
+/*   Updated: 2021/04/10 23:32:23 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 #include "mlx.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(void)
 {
-	t_data	meta;
+	t_meta	meta;
 	void	*mlx;
 	void	*window;
 	
-	meta = (t_data) {.img = NULL, .addr = NULL, bpp = 0, length = 0, endian = 0};
+	meta = (t_meta) {.img = NULL, .addr = NULL, .bpp = 0, .length = 0, .endian = 0};
+	printf("%s\n", meta.img);
+
 	mlx = mlx_init();
 	window = mlx_new_window(mlx, 800, 600, "miniRT");
 	mlx_loop(window);
