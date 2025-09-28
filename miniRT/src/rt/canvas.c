e9790d73785e09f388f4030197947998776700f1
Dmitry Mikhailenko <d@mikhailenko.net>
2025-09-28 16:05:41 +0200

    implement perspective view

--- miniRT/src/rt/canvas.c
+++ miniRT/src/rt/canvas.c
@@ -6,7 +6,7 @@
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 02:29:22 by rmander           #+#    #+#             */
-/*   Updated: 2021/06/04 07:11:41 by rmander          ###   ########.fr       */
+/*   Updated: 2021/06/07 03:12:49 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,38 @@
 #include <math.h>
 #include <stdlib.h>
 #include <errno.h>
+#include "libft.h"
+
+static t_vector3	get_dirvec(t_vector3 *viewpoint)
+{
+	t_vector3	basis[3];
+	t_vector3	xu;
+	t_vector3	yv;
+	t_vector3	dw;
+	t_vector3	dirvec;
+
+	basis[0] = (t_vector3){1, 0, 0};
+	basis[1] = (t_vector3){0, 1, 0};
+	basis[2] = (t_vector3){0, 0, -1};
+	xu = cmultvec3(viewpoint->x, &basis[0]);
+	yv = cmultvec3(viewpoint->y, &basis[1]);
+	dw = cmultvec3(viewpoint->z, &basis[2]);
+	dirvec = sumvec3(&xu, &yv);
+	dirvec = sumvec3(&dirvec, &dw);
+	return (dirvec);
+}
 
 t_viewport	calc_viewport(t_data *data, t_camera *cam)
 {
 	t_viewport	viewport;
+	double		fov;
 
-	viewport.width = 2 * tan(deg_to_rad(cam->fov * pow (2, -1)));
-	viewport.height = (data->screen->height
-			* (viewport.width * pow(data->screen->width, -1)));
+	fov = cam->fov;
+	if (ft_fequal(cam->fov, 180))
+		fov -= 1e-2;
+	viewport.width = 2 * tan(deg_to_rad(fov / 2));
+	viewport.height = data->screen->height
+		* (viewport.width / (double)data->screen->width);
 	return (viewport);
 }
 
@@ -36,12 +60,14 @@ t_viewport	calc_viewport(t_data *data, t_camera *cam)
 t_vector3	canvas_to_viewport(t_data *data, t_camera *cam, int x, int y)
 {
 	t_vector3	dirvec;
-	double		scale;
+	t_vector3	viewpoint;
 
-	scale = tan(deg_to_rad(cam->fov * pow (2, -1)));
-	dirvec.x = x * (cam->viewport->width * pow(data->screen->width, -1));
-	dirvec.y = y * (cam->viewport->height * pow(data->screen->height, -1));
-	dirvec.z = cam->viewport->height * pow(2, -1) * pow(scale, -1);
+	viewpoint.x = (cam->viewport->width / (double)data->screen->width)
+		* (x + 0.5);
+	viewpoint.y = (cam->viewport->height / (double)data->screen->height)
+		* (y + 0.5);
+	viewpoint.z = -1;
+	dirvec = get_dirvec(&viewpoint);
 	return (normvec3(&dirvec));
 }
 
