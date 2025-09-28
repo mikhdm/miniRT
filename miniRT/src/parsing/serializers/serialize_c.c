0dd7faa0fcef1d46c9773d0d3072a521f73c2236
Dmitry Mikhailenko <d@mikhailenko.net>
2025-09-28 16:05:39 +0200

    serialize_c

--- miniRT/src/parsing/serializers/serialize_c.c
+++ miniRT/src/parsing/serializers/serialize_c.c
@@ -6,7 +6,7 @@
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 03:59:45 by rmander           #+#    #+#             */
-/*   Updated: 2021/05/29 23:01:47 by rmander          ###   ########.fr       */
+/*   Updated: 2021/05/29 23:02:01 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_data  *serialize_c(t_data *data, char const *line)
 {
-	char	**strs;
+	char		**strs;
 	t_camera	*cam;
 	return (data);
 }
